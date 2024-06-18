6afecc44a07807992939d44e86b411e806d034c8
ziyad <ziyad.alnawfal1@gmail.com>
2024-06-18 22:50:38 +0100

    current progress

---
+++ app.js
@@ -0,0 +1,5 @@
+projectsbutton = document.getElementById("Projectsbutton")
+arrow = document.getElementById("arrow")
+projectsbutton.addEventListener("click",function(){
+    arrow.classList.add("arrowmovement")
+});
