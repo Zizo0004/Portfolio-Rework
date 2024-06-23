c69dfa592bb20f0373961a118c766672e78ca488
ziyad <ziyad.alnawfal1@gmail.com>
2024-06-23 19:15:34 +0100

    added all the projects to be displayed, fixed the animation sequence

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const callback = (entries,observer)=>{
-    console.log("HHELLLLOOOO")
     for(entry of entries){
-        if(entry.isIntersecting){
+        if(entry.isIntersecting && entry.target.id == "projects"){
             entry.target.classList.add("projectsanimation")
             observer.unobserve(entry.target)
         }
+        
+        else if(entry.isIntersecting && entry.target.nodeName == "IMG"){
+            entry.target.src = entry.target.alt
+            entry.target.classList.add("blurclass")
+            observer.unobserve(entry.target)
+        }
         else{
-            console.log(`${entry} is NOT intersecting`)
+            console.log(`img not found src: ${entry.target.src}`)
+
         }
     }
 }
 const observer = new IntersectionObserver(callback)
 target = document.getElementById("projects")
+imgs = document.querySelectorAll("img")
+for(img of imgs){
+    observer.observe(img)
+}
 observer.observe(target)
